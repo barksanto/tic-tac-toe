c0b36d2d2664ee7ed905c9a4f6101d9d8785a838
Barkley Santo <barkley.santo@gmail.com>
2021-03-27 13:09:06 -0600

    select all boxes

---
+++ index.js
@@ -0,0 +1,14 @@
+// select each box
+const one = document.querySelector('.one'),
+  two = document.querySelector('.two'),
+  three = document.querySelector('.three'),
+  four = document.querySelector('.four'),
+  five = document.querySelector('.five'),
+  six = document.querySelector('.six'),
+  seven = document.querySelector('.seven'),
+  eight = document.querySelector('.eight'),
+  nine = document.querySelector('.nine');
+
+
+
+
